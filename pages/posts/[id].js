35a9cb4770035d6d56d8296922fec002a2db9b58
yudwig <yudwig.dev@gmail.com>
2021-03-10 18:26:38 +0900

    Modify post view

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,8 @@
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from 'next/head';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 
 export default function Post({ postData }) {
   return (
@@ -8,14 +10,14 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-    </Layout>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+   </Layout>
   );
 }
 
@@ -35,3 +37,4 @@ export async function getStaticProps({ params }) {
     }
   };
 }
+
